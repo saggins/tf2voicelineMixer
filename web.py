fb5221951189d25b4a0d5503fd68009b9dcd9378
Saggins <win32prog@gmail.com>
2022-02-27 22:18:20 -0800

    IPA -> APRABET

--- web.py
+++ web.py
@@ -9,5 +9,5 @@ app.mount('/', StaticFiles(directory='./web/public', html=True), name="web")
 async def handleUser(input: UserInput):
     #TODO This function in brains; make do brainstuff
     #TODO Need to report to data.py
-    #TODO Need to analze phome in this function
+    #TODO Need to analze phome in this function (*Aprabet*)
     #TODO Need to return Phome models, word models to client  
